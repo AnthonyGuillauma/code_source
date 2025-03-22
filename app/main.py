db28c6f0183845a81e711eb62a9e0b579d54a252
AnthonyGuillauma <guillaumaanthony@gmail.com>
2025-03-22 22:02:50 +0100

    Feat: Ajout de l'analyse syntaxique de fichier log - Ajout de la méthode parse_fichier de ParseurLogApache qui permet de  parser un fichier de log Apache - Ajout de la méthode parse_entree de ParseurLogApache qui permet de parser une entrée d'un fichier de log Apache - Ajout de get_information_entree de ParseurLogApache qui permet de   récupérer la valeur d'une information dans une entrée - Ajout de FormatLogApacheInvalideException qui représente une erreur   lors de l'analyse syntaxique d'un fichier log - Modification du fichier main.py pour appeller la fonction parse_entree   et pour afficher un message en cas d'erreur dans l'analyse

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ Point d'entrée de l'application LogBuster !
 
 import colorama
 from cli.parseur_arguments_cli import ParseurArgumentsCLI, ArgumentCLIException
+from parse.parseur_log_apache import ParseurLogApache, FormatLogApacheInvalideException
 
 
 if __name__ == "__main__":
@@ -25,7 +26,13 @@ if __name__ == "__main__":
         parseur_cli = ParseurArgumentsCLI()
         arguments_cli = parseur_cli.parse_args()
         # Analyse syntaxique du fichier log
+        parseur_log = ParseurLogApache(arguments_cli.chemin_log)
+        parseur_log.parse_fichier()
         # Analyse statistique du fichier log
         # Exportation de l'analyse
     except ArgumentCLIException as ex:
         print(f"Erreur dans les arguments fournis !\n {ex}")
+    except FileNotFoundError as ex:
+        print(f"Erreur dans la recherche du log Apache !\n{ex}")
+    except FormatLogApacheInvalideException as ex:
+        print(f"Erreur dans l'analyse du log Apache !\n{ex}")
