0b035ccd1843075cbe96a079acc8a18289a7d9d2
AnthonyGuillauma <guillaumaanthony@gmail.com>
2025-03-26 19:34:48 +0100

    Feat: Modification des types attendus dans RequeteInformations
    - Modification des types attendus dans la classe RequeteInformations
      pour les attributs methode_http, url et protocole_http qui sont
      maintenant autorisée à être None

--- app/donnees/requete_informations.py
+++ app/donnees/requete_informations.py
@@ -9,13 +9,20 @@ from datetime import datetime
 @dataclass
 class RequeteInformations:
     """
-    Représente les informations de la requête HTTP dans un log Apache.
+    Représente les informations de la requête HTTP à partir d'une entrée d'un log Apache.
+
+    Cette classe regroupe les données extraites d'une entrée de log,
+    qui concernent les informations techniques sur la requête émise au
+    serveur Apache.
 
     Attributes:
         horodatage (datetime): L'horodatage de la requête.
-        methode_http (str): La méthode HTTP utilisée.
-        url (str): L'URL cible de la requête.
-        protocole_http (str): Le protocole HTTP utilisé.
+        methode_http (Optional[str]): La méthode HTTP utilisée.
+            Peut être None si non fournie.
+        url (Optional[str]): L'URL cible de la requête.
+            Peut être None si non fournie.
+        protocole_http (Optional[str]): Le protocole HTTP utilisé.
+            Peut être None si non fournie.
         ancienne_url (Optional[str]): L'URL de provenance (referrer).
             Peut être None si non fournie.
 
@@ -23,9 +30,9 @@ class RequeteInformations:
         TypeError: Si les attributs ne sont pas du type attendu ou None.
     """
     horodatage: datetime
-    methode_http: str
-    url: str
-    protocole_http: str
+    methode_http: Optional[str]
+    url: Optional[str]
+    protocole_http: Optional[str]
     ancienne_url: Optional[str]
 
     def __post_init__(self):
@@ -33,14 +40,14 @@ class RequeteInformations:
         if not isinstance(self.horodatage, datetime):
             raise TypeError("L'horodatage doit être de type datetime.")
         # Vérification de la méthode HTTP
-        if not isinstance(self.methode_http, str):
-            raise TypeError("La méthode HTTP doit être une chaine de caractère.")
+        if self.methode_http != None and not isinstance(self.methode_http, str):
+            raise TypeError("La méthode HTTP doit être une chaine de caractère ou None.")
         # Vérification de la ressource demandée
-        if not isinstance(self.url, str):
-            raise TypeError("L'URL doit être une chaine de caractère.")
+        if self.url != None and not isinstance(self.url, str):
+            raise TypeError("L'URL doit être une chaine de caractère ou None.")
         # Vérification du protocole HTTP
-        if not isinstance(self.protocole_http, str):
-            raise TypeError("Le protocole HTTP doit être une chaine de caractère.")
+        if self.protocole_http != None and not isinstance(self.protocole_http, str):
+            raise TypeError("Le protocole HTTP doit être une chaine de caractère ou None.")
         # Vérification de l'ancienne URL
         if self.ancienne_url != None and not isinstance(self.ancienne_url, str):
             raise TypeError("L'ancienne URL doit être une chaine de caractère ou None.")
