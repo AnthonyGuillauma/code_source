171d3d5de77b03cd5e020d851e64e358a3d5d4f5
AnthonyGuillauma <guillaumaanthony@gmail.com>
2025-04-03 19:15:23 +0200

    Refractor: Amélioratio de la vérification de lancement de chargement
    - Amélioration de la condition qui vérifier que l'animation n'est pas
      déjà lancée lorsque on lance une nouvelle animation de chargement

--- app/cli/afficheur_cli.py
+++ app/cli/afficheur_cli.py
@@ -3,12 +3,12 @@ Module pour les intéractions avec la ligne de commande.
 """
 
 import sys
+from pathlib import Path
 from json import load
 from time import sleep
 from random import choice
 import colorama
 import threading
-
 
 class AfficheurCLI:
     """
@@ -18,8 +18,10 @@ class AfficheurCLI:
         COULEUR_MESSAGE_NORMAL (str): La couleur pour les messages normaux en CLI.
         COULEUR_MESSAGE_ERREUR (str): La couleur pour les messages d'erreur en CLI.
         _thread_chargement (Union[None,Thread]): Le thread de l'animation de chargement.
-        _thread_chargement_demande_arret (Event): L'évènement pour demander au thread de
-            l'animation de chargement de s'arrêter.
+        _thread_chargement_termine (Event): L'évènement pour demander au thread de
+            l'animation de chargement de s'arrêter lorsque le chargement est terminé.
+        _thread_chargement_erreur (Event): L'évènement pour demander au thread de
+            l'animation de chargement de s'arrêter lorsque une erreur s'est produite.
         _animations_actuelles (dict): Les éléments visuels pour l'animation de chargement.
     """
     COULEUR_MESSAGE_NORMAL = colorama.Fore.WHITE
@@ -29,11 +31,16 @@ class AfficheurCLI:
         """
         Initialise un objet pour afficher des informations dans la ligne de commande.
         """
+        # Normalise les codes couleurs pour fonctionner partout
+        colorama.init()
         # Initialise les variables pour le chargement
         self._thread_chargement = None
-        self._thread_chargement_demande_arret = threading.Event()
+        self._thread_chargement_termine = threading.Event()
+        self._thread_chargement_erreur = threading.Event()
         # Récupère les animations
-        with open("assets/animations.json", "r", encoding="utf-8") as animations:
+        chemin_racine = Path(__file__).parent.parent.parent.resolve()
+        chemin_animations = chemin_racine / "assets" / "animations.json"
+        with open(chemin_animations, "r", encoding="utf-8") as animations:
             elements_animations = load(animations)
         # Choisis une animation au hasard parmi chaque catégorie d'animation
         self._animations_actuelles = {
@@ -105,7 +112,7 @@ class AfficheurCLI:
         if not isinstance(exception, Exception):
             raise TypeError("L'exception à afficher doit être une instance de Exception.")
         # Ecriture du message
-        print(self.COULEUR_MESSAGE_ERREUR + f"{message}\n{exception}", flush=True, end="\r")
+        print(self.COULEUR_MESSAGE_ERREUR + f"{message}\n{exception}", flush=True)
 
     def lance_animation_chargement(self):
         """
@@ -116,16 +123,16 @@ class AfficheurCLI:
             None
         """
         # Si le thread est déjà lancé, annulation de l'animation
-        if self._thread_chargement and self._thread_chargement.is_alive():
-            return
-        # On réinitialise la demande d'arrêt
-        self._thread_chargement_demande_arret.clear()
-        # Initialisation du thread pour le chargement
-        self._thread_chargement = threading.Thread(target=self.__animation_chargement)
-        # Lancement du thread pour le chargement
-        self._thread_chargement.start()
+        if self._thread_chargement is None:
+            # On réinitialise les demandes d'arrêt
+            self._thread_chargement_termine.clear()
+            self._thread_chargement_erreur.clear()
+            # Initialisation du thread pour le chargement
+            self._thread_chargement = threading.Thread(target=self._animation_chargement, daemon=True)
+            # Lancement du thread pour le chargement
+            self._thread_chargement.start()
         
-    def __animation_chargement(self):
+    def _animation_chargement(self):
         """
         Lance l'animation de chargement en boucle jusqu'à la demande d'arrêt via
         l'attribut :attr:`_thread_chargement_demande_arret`.
@@ -139,36 +146,51 @@ class AfficheurCLI:
         signes_rayon_laser = self._animations_actuelles["rayon_laser"]
         couleurs = ["\033[91m", "\033[93m", "\033[94m", "\033[95m"] # Rouge, Jaune, Bleu, Magenta
         
-        # Eléments de l'animation de fin de chargement
+        # Eléments de l'animation de fin de chargement en cas de succès
         chasseur_gagne = self._animations_actuelles["chasseur"][2]
         fantome_perd = self._animations_actuelles["fantome"][1]
+
+        # Eléments de l'animation de fin de chargement en cas d'erreur
+        chasseur_perd = self._animations_actuelles["chasseur"][1]
+        fantome_gagne = self._animations_actuelles["fantome"][2]
 
         # Variables pour l'animation de chargement
         index_boucle = 0
         rayon_laser = ""
 
         # Début de l'animation (jusqu'à la demande d'arrêt)
-        while not self._thread_chargement_demande_arret.is_set():
-            # Récupération de la prochaine couleur
-            couleur_courante = couleurs[(index_boucle % len(couleurs))]
-            # Récupération du prochain signe du rayon
-            signe_courant = signes_rayon_laser[(index_boucle % len(signes_rayon_laser))]
-            # Ajout du dernier signe avec la nouvelle couleur au rayon
-            rayon_laser += couleur_courante + signe_courant
-            # Réactualisation de l'animation de chargement
-            self.reecrire_ligne(f"{chasseur_chargement}{rayon_laser}\033[0m{fantome_chargement}")
-            index_boucle += 1
+        while not (self._thread_chargement_termine.is_set()
+                   or self._thread_chargement_erreur.is_set()):
+            # Arrête d'ajouter des caractères lorsque la chaîne est trop longue
+            if (index_boucle < 40):
+                # Récupération de la prochaine couleur
+                couleur_courante = couleurs[(index_boucle % len(couleurs))]
+                # Récupération du prochain signe du rayon
+                signe_courant = signes_rayon_laser[(index_boucle % len(signes_rayon_laser))]
+                # Ajout du dernier signe avec la nouvelle couleur au rayon
+                rayon_laser += couleur_courante + signe_courant
+                # Réactualisation de l'animation de chargement
+                self.reecrire_ligne(f"{chasseur_chargement}{rayon_laser}\033[0m{fantome_chargement}")
+                index_boucle += 1
             sleep(0.05)
 
         # Suppression de la ligne de chargement
         self.reecrire_ligne("\033[K")
-        # Message d'animation terminée
-        self.reecrire_ligne(f"{chasseur_gagne}{" " * index_boucle}\033[0m{fantome_perd}")
+        if (self._thread_chargement_termine.is_set()):
+            # Message d'animation terminée
+            self.reecrire_ligne(f"{chasseur_gagne}{" " * index_boucle}\033[0m{fantome_perd}\n")
+            self.affiche_message(f"Analyse terminée! We came, we saw, we logged it.")
+        else:
+            # Message d'animation erreur
+            self.reecrire_ligne(f"{chasseur_perd}{" " * index_boucle}\033[0m{fantome_gagne}\n")
 
-    def stop_animation_chargement(self):
+    def stop_animation_chargement(self, erreur: bool = False):
         """
         Lance une demande d'arrêt au thread qui gère l'animation de chargement
         en cours. Si aucune animation n'est en cours, cette méthode ne fait rien.
+
+        Args:
+            erreur (bool): Indique si la demande d'arrêt est dûe à une erreur ou non.
 
         Returns:
             None
@@ -176,7 +198,10 @@ class AfficheurCLI:
         # Si le thread de chargement existe et est lancé
         if self._thread_chargement and self._thread_chargement.is_alive():
             # Lancement de la demade d'arrêt
-            self._thread_chargement_demande_arret.set()
+            if not erreur:
+                self._thread_chargement_termine.set()
+            else:
+                self._thread_chargement_erreur.set()
             # Attente de l'arrêt depuis le thread principal
             self._thread_chargement.join()
-            self.affiche_message(f"\nAnalyse terminée! We came, we saw, we logged it.")
+            self._thread_chargement = None
