c3132b83747d23c66603fa3169991a1720db0418
AnthonyGuillauma <guillaumaanthony@gmail.com>
2025-04-06 11:02:29 +0200

    Feat: Modification de la sortie dans Exporteur
    - Ajout de la base de la méthode d'export d'histogramme HTML
    - Modification de la vérification de la sortie, qui doit maintenant
      être un dossier
    - Modification des méthodes de l'exporteur qui écrivent maintenant le
      fichier spécifié dans le dossier de la sortie

--- app/export/exporteur.py
+++ app/export/exporteur.py
@@ -2,7 +2,7 @@
 Module pour l'exportation des données.
 """
 
-from os.path import abspath, dirname, isdir
+from os.path import abspath, isdir, join
 from json import dump
 
 
@@ -12,62 +12,30 @@ class Exporteur:
     vers un fichier de sortie.
 
     Attributes:
-        _chemin_sortie (str): Le chemin du fichier vers lequel
-                les données seront exportées.
+        _chemin_sortie (str): Le chemin du dossier vers lequel les données
+            vont être exportées.
     """
 
     def __init__(self, chemin_sortie: str):
-        """
-        Initialise un exporteur de données.
-
-        Args:
-            chemin_sortie (str): Le chemin du fichier vers lequel
-                les données seront exportées.
-        
-        Raises:
-            TypeError: Le chemin de sortie n'est pas une chaîne de caractère.
-            ExportationDossierParentException: Exportation impossible à cause de
-                l'inexistance du dossier parent du fichier d'exportation.
-        """
-        # Vérification du type du paramètre
-        if not isinstance(chemin_sortie, str):
-            raise TypeError("Le chemin de sortie doit être une chaîne de caractère.")
-        # Vérification du chemin d'exportation
-        self.verification_exportation_possible(chemin_sortie)
-        # Ajout du chemin d'exportation
-        self._chemin_sortie = chemin_sortie
-
-    def verification_exportation_possible(self, chemin_sortie: str) -> None:
-        """
-        Vérifie qu'une exportation est possible vers le chemin du fichier indiqué. Renvoie une
-        exception expliquant le problème si elle n'est pas possible.
-
-        Args:
-            chemin_sortie (str): Le chemin du fichier d'exportation.
-
-        Returns:
-            None
-
-        Raises:
-            ExportationDossierParentException: Le dossier parent du fichier n'existe pas.
-        """
-        # Vérification du type du paramètre
+        # Vérification du paramètre
         if not isinstance(chemin_sortie, str):
             raise TypeError("Le chemin de sortie doit être une chaîne de caractères.")
         # Vérification du chemin
         chemin_sortie_absolue = abspath(chemin_sortie)
-        dossier_parent = dirname(chemin_sortie_absolue)
-        if not isdir(dossier_parent):
-            raise ExportationDossierParentException(f"Impossible d'exporter vers le "
-                                       f"fichier {chemin_sortie}, son dossier parent "
-                                       f"{dossier_parent} n'existe pas.")
+        if not isdir(chemin_sortie_absolue):
+            raise ExportationDossierIntrouvableException(f"Impossible d'exporter vers le "
+                                       f"dossier {chemin_sortie} ({chemin_sortie_absolue}), "
+                                        "le dossier n'existe pas.")
+        # Ajout du chemin
+        self._chemin_sortie = chemin_sortie
 
-    def export_vers_json(self, donnees: dict) -> None:
+    def export_vers_json(self, donnees: dict, nom_fichier: str) -> None:
         """
-        Export le dictionnaire fourni vers le :attr:`chemin de sortie`.
+        Export le dictionnaire fourni vers le ``chemin de sortie``.
 
         Args:
             donnees (dict): Le dictionnaire qui contient les données.
+            nom_fichier (str): Le nom du fichier JSON.
 
         Returns:
             None
@@ -76,24 +44,60 @@ class Exporteur:
             TypeError: Le paramètre ``donnees`` n'est pas un dictionnaire.
             ExportationException: Une erreur lors de l'écriture dans le fichier JSON.
         """
-        # Vérification du type du paramètre
+        # Vérification du type des paramètres
         if not isinstance(donnees, dict):
-            raise TypeError("Les données à exporter doivent être sous une forme "
+            raise TypeError("Les statistiques à exporter doivent être sous une forme "
             "de dictionnaire.")
+        if not isinstance(nom_fichier, str):
+            raise TypeError("Le nom du fichier doit être une chaîne de caractère.")
+        # Vérification du nom du fichier
+        if not nom_fichier.endswith(".json"):
+            raise ValueError("Le fichier JSON doit terminé par l'extention '.json'.")
         # Exportation
+        chemin_fichier = join(self._chemin_sortie, nom_fichier)
         try:
-            with open(self._chemin_sortie, 'w', encoding="utf-8") as fichier:
+            with open(chemin_fichier, 'w', encoding="utf-8") as fichier:
                 dump(donnees, fichier, indent=4)
         except Exception as ex:
             raise ExportationException(str(ex)) from ex
+    
+    def export_vers_html_histogramme(self, donnees: list, nom_fichier: str) -> None:
+        """
+        Export la liste fournie vers une image d'un histogramme vers le ``chemin de sortie``.
+
+        Args:
+            nom_fichier (str): Le nom du fichier HTML.
+        """
+        # Vérification du type des paramètres
+        if not isinstance(donnees, list):
+            raise TypeError("L'histogramme à exporter doit être sous une forme de liste.")
+        if not isinstance(nom_fichier, str):
+            raise TypeError("Le nom du fichier doit être une chaîne de caractère.")
+        # Vérification du nom du fichier
+        if not nom_fichier.endswith(".html"):
+            raise ValueError("Le fichier HTML doit terminé par l'extention '.html'.")
+        # Exportation
+        chemin_fichier = join(self._chemin_sortie, nom_fichier)
+
 
 class ExportationException(Exception):
     """
     Représente une erreur lors de l'exportation de données.
     """
 
-class ExportationDossierParentException(ExportationException):
+class ExportationJsonException(ExportationException):
+    """
+    Représente une erreur lors de l'exportation de données vers un format JSON.
+    """
+
+class ExportationHtmlHistogrammeException(ExportationException):
+    """
+    Représente une erreur lors de l'exportation de données vers un histogramme
+    au format HTML.
+    """
+
+class ExportationDossierIntrouvableException(ExportationException):
     """
     Représente une erreur lorsque une exportation est impossible
-    lorsque le dossier parent du fichier d'exportation n'existe pas.
+    lorsque le dossier de l'exportation n'existe pas.
     """
